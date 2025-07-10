bfcc23693690781014cbbe4f90723307c8e6315e
pibrei <thepibrei@gmail.com>
2025-07-10 17:30:54 -0300

    Preparação para deploy no GitHub Pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/nome-do-seu-repositorio/', // Substitua pelo nome real do seu repositório
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
